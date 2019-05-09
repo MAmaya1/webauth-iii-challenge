e032949d9a1b8e5dfe82cfdb0d83e12de9ad2ac4
Mario Amaya <thefozcat@gmail.com>
2019-05-09 14:30:46 -0700

    styled login

--- login-page/src/components/Login.js
+++ login-page/src/components/Login.js
@@ -4,6 +4,64 @@ import { Link } from 'react-router-dom';
 
 import { login } from '../actions';
 
+import styled from 'styled-components';
+
+// Styled Components
+
+const LoginPage = styled.div`
+    display: flex;
+    flex-direction: column;
+    max-width: 700px;
+    margin: 200px auto;
+    background: white;
+    padding: 20px 30px;
+    text-align: center;
+`
+
+const LoginForm = styled.form`
+    display: flex;
+    flex-direction: column;
+
+    input {
+        width: 40%;
+        margin: 5px auto;
+        padding: 5px;
+    }
+    
+    button {
+        height: 28px;
+        width: 100px;
+        background: white;
+        border: solid 1px grey;
+        cursor: pointer;
+        margin: 10px auto;
+
+        &:hover {
+            background: lightgrey;
+            transition: all 200ms ease;
+        }
+    }
+
+    a {
+        color: blue;
+        text-decoration: none;
+
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+
+    .err {
+        color: red;
+        position: absolute;
+        text-align: center;
+        margin: 123px 299px;
+        font-size: 0.8rem;
+    }
+`
+
+// Login Component Constructor
+
 class Login extends React.Component {
     state = {
         credentials: {
@@ -31,9 +89,9 @@ class Login extends React.Component {
 
     render() {
         return (
-            <div className="login">
+            <LoginPage>
                 <h1>Log In</h1>
-                <form>
+                <LoginForm>
                     <input
                         type="text"
                         name="username"
@@ -46,13 +104,13 @@ class Login extends React.Component {
                         value={this.state.password}
                         onChange={this.handleChange}
                     />
-                    {this.props.logInError && (<p>{this.props.logInError}</p>)}
+                    {this.props.logInError && (<p className="err">{this.props.logInError}</p>)}
                     <button onClick={this.login}>
                         {this.state.loggingIn ? ('Logging in...') : ('Login')}
                     </button>
                     <p>Not a registered user? <Link to="/register">Sign Up</Link></p>
-                </form>
-            </div>
+                </LoginForm>
+            </LoginPage>
         )
     }
 }
