3c9540828e0f0d7cc178b3f678c8be6366add82e
Mario Amaya <thefozcat@gmail.com>
2019-05-08 17:24:10 -0700

    created UsersView

--- login-page/src/App.js
+++ login-page/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Login from './components/Login';
+import UsersView from './views/users-view';
 
 function App() {
   return (
     <Router>
       <Route exact path='/' component={Login}/>
+      <PrivateRoute path='/protected' component={UsersView}/>
     </Router>
   );
 }
---
+++ login-page/src/views/users-view.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class UsersView extends React.Component {
+
+    /* componentDidMount() {
+        Coming soon!
+    } */
+
+    render() {
+        return (
+            <div className="users-view">
+                <h2>Users</h2>
+            </div>
+        )
+    }
+}
+
+export default UsersView;
